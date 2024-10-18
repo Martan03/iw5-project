6cd0e050fb86bba3a4f9646a293fa37c25deeea1
Jan Krahula <honza.krahula2003@gmail.com>
2024-10-18 21:22:47 +0200

    Added common models

---
+++ IW5Forms.Common.Models/AnswerModel.cs
@@ -0,0 +1,13 @@
+﻿namespace IW5Forms.Common.Models
+{
+    public record AnswerModel
+    {
+        public required Guid Id { get; set; }
+        
+        // Users answer
+        public required string Text { get; set; }
+
+        // UserId is nullable, in case incognito mode is enabled
+        public Guid? UserId { get; set; }
+    }
+}
---
+++ IW5Forms.Common.Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿namespace IW5Forms.Common.Models
+{
+    public enum UserRoles
+    {
+        User,
+        Admin
+    }
+    public record UserModel
+    {
+        public required Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? PhotoUrl { get; set; }
+
+        // Role is either normal user (User) or Admin
+        public required UserRoles Role { get; set; }
+        
+        // Forms contain all accessible forms by this user and bool whether the form was filled by the user
+        public IDictionary<FormModel, bool> Forms { get; set; } = new Dictionary<FormModel, bool>();
+    }
+}
