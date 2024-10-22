920b92b8543b95e7f844c1940d9f8c853f4f3827
adam <a.zatloukal.19@student.gymzl.cz>
2024-10-22 04:31:45 +0300

    basic structure for api added

--- IW5Forms.Api.App/Program.cs
+++ IW5Forms.Api.App/Program.cs
@@ -1,3 +1,9 @@
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using IW5Forms.Common.Extentions;
+using ServiceCollectionExtensions = IW5Forms.Common.Extentions.ServiceCollectionExtensions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Localization;
 
 namespace IW5Forms.Api.App
 {
@@ -5,32 +11,123 @@ namespace IW5Forms.Api.App
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder();
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            ConfigureCors(builder.Services);
+            ConfigureOpenApiDocuments(builder.Services);
+            ConfigureDependencies(builder.Services, builder.Configuration);
+            ConfigureAutoMapper(builder.Services);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            ValidateAutoMapperConfiguration(app.Services);
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
+            UseDevelopmentSettings(app);
+            UseSecurityFeatures(app);
+            UseRouting(app);
+            UseEndpoints(app);
+            UseOpenApi(app);
 
             app.Run();
+        }
+
+        private static void ConfigureCors(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddCors(options =>
+            {
+                options.AddDefaultPolicy(o =>
+                    o.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+            });
+        }
+
+        private static void ConfigureOpenApiDocuments(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddEndpointsApiExplorer();
+            serviceCollection.AddOpenApiDocument();
+        }
+
+        private static void ConfigureDependencies(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            //EF stuff
+
+            ServiceCollectionExtensions.AddInstaller<ApiBLInstaller>(serviceCollection);
+        }
+
+        private static void ConfigureAutoMapper(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddAutoMapper(typeof(ApiBLInstaller));
+        }
+
+        private static void ValidateAutoMapperConfiguration(IServiceProvider serviceProvider)
+        {
+            var mapper = serviceProvider.GetRequiredService<IMapper>();
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        private static void UseEndpoints(WebApplication application)
+        {
+            var endpointsBase = application.MapGroup("api")
+                .WithOpenApi();
+
+            UseSearchEndpoints(endpointsBase);
+            UseUserEndpoints(endpointsBase);
+            UseFormEndpoints(endpointsBase);
+            UseAnswerEndpoints(endpointsBase);
+            UseQuestionEndpoints(endpointsBase);
+        }
+
+        private static void UseSearchEndpoints(RouteGroupBuilder routeGroupBuilder)
+        {
+         
+        }
+
+        private static void UseUserEndpoints(RouteGroupBuilder routeGroupBuilder)
+        {
+
+        }
+
+        private static void UseFormEndpoints(RouteGroupBuilder routeGroupBuilder)
+        {
+
+        }
+
+        private static void UseAnswerEndpoints(RouteGroupBuilder routeGroupBuilder)
+        {
+
+        }
+
+        private static void UseQuestionEndpoints(RouteGroupBuilder routeGroupBuilder)
+        {
+
+        }
+
+        private static void UseDevelopmentSettings(WebApplication application)
+        {
+            var environment = application.Services.GetRequiredService<IWebHostEnvironment>();
+
+            if (environment.IsDevelopment())
+            {
+                application.UseDeveloperExceptionPage();
+            }
+        }
+
+        private static void UseSecurityFeatures(IApplicationBuilder application)
+        {
+            application.UseCors();
+            application.UseHttpsRedirection();
+        }
+
+        private static void UseRouting(IApplicationBuilder application)
+        {
+            application.UseRouting();
+        }
+
+        private static void UseOpenApi(IApplicationBuilder application)
+        {
+            application.UseOpenApi();
+            application.UseSwaggerUi();
         }
     }
 }
---
+++ IW5Forms.Api.BL/Installers/ApiBLInstaller.cs
@@ -0,0 +1,16 @@
+﻿using IW5Forms.Common.BL.Facades;
+using IW5Forms.Common.Installers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+public class ApiBLInstaller : IInstaller
+{
+    public void Install(IServiceCollection serviceCollection)
+    {
+        serviceCollection.Scan(selector =>
+            selector.FromAssemblyOf<ApiBLInstaller>()
+                    .AddClasses(classes => classes.AssignableTo<IAppFacade>())
+                    .AsSelfWithInterfaces()
+                    .WithScopedLifetime());
+    }
+}
