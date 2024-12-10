a18c695c666e7cb91459e9ed4fd471117e0a6b54
Martan03 <martinslezak03@gmail.com>
2024-12-10 09:25:36 +0100

    Remove second http client

--- IW5Forms.Web.App/Program.cs
+++ IW5Forms.Web.App/Program.cs
@@ -31,11 +31,10 @@ builder.Services.AddHttpClient("api", client => client.BaseAddress = new Uri(api
 
 builder.Services.AddScoped<HttpClient>(serviceProvider => serviceProvider.GetService<IHttpClientFactory>().CreateClient("api"));
 
-
-builder.Services.AddScoped(sp => new HttpClient
-{
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-});
+// builder.Services.AddScoped(sp => new HttpClient
+// {
+//     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+// });
 builder.Services.AddAutoMapper(configuration =>
 {
     // This is a temporary fix - should be able to remove this when version 11.0.2 comes out
